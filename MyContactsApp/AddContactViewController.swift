d627889fc8e098d11c46d20849b9f3d08fbb2837
Tiago Pereira <tiago@Tiagos-MacBook-Pro-2.local>
2018-03-14 23:35:29 +0100

    Intial navigation flow setup

---
+++ MyContactsApp/AddContactViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddContactViewController.swift
+//  MyContactsApp
+//
+//  Created by Tiago Pereira on 14/03/18.
+//  Copyright © 2018 Tiago Pereira. All rights reserved.
+//
+
+import UIKit
+
+class AddContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
